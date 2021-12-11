e8235500e4f39a4f294fb130e0a520680ab5f5a9
neya-java <sergey.arlashkin@gmail.com>
2021-12-11 18:53:32 +0300

    Create Homework Lesson4

--- src/main/java/lesson4/TestMain4.java
+++ src/main/java/lesson4/TestMain4.java
@@ -4,7 +4,7 @@ import java.util.*;
 import java.util.LinkedList;
 
 public class TestMain4 {
-
+//  Не успеваю
     private static TwoSideLinkedList<Integer> linkedList;
 
     public static void main(String[] args) {
